a81c88f2815167f421b3efddeeace97103a394f2
cgbridgewater <cgbridgewater@outlook.com>
2025-01-09 13:24:12 -0800

    move link hrefs to .env

--- controllers/mail.controller.js
+++ controllers/mail.controller.js
@@ -33,7 +33,7 @@ const sendMail = async (req, res) => {
             from: process.env.EMAIL_USER, // Sender's email (must match the auth email)
             to: process.env.EMAIL_USER, // Recipient (also the sender for this case)
             bcc: mailData.contact, // Optionally send a copy to the client (contact email)
-            subject: `Seniors Mobile Tax Services Message Confirmation - Email from ${mailData.userName} | ${mailData.subject}`, // Email subject
+            subject: `Seniors Mobile Tax Services Message Confirmation - Email from ${ mailData.userName } | ${ mailData.subject }`, // Email subject
             html: // Email HTML content (email body structure)
                 `
                 <body style="background-color: black; margin: 0; padding: 0;">
@@ -43,12 +43,12 @@ const sendMail = async (req, res) => {
                                 <table role="presentation" width="100%" cellspacing="0" cellpadding="0" style="background-color: #3C3B6E;">
                                     <tr>
                                         <td style="padding: 10px; text-align: left;">
-                                            <a href="https://valor-22.beedev-services.com/home">
+                                            <a href=${ process.env.SMTS_Link }>
                                                 <img src="cid:smts_logo" alt="SMTS Icon" style="height: 80px; background-color: #DDDDE0; border-radius: 50%; padding: 2px;">
                                             </a>
                                         </td>
                                         <td style="padding: 10px; text-align: right;">
-                                            <p style="color: #DDDDE0; font-size: 1rem;">Visit <a href="https://valor-22.beedev-services.com/home" style="text-decoration: underline; color: #DDDDE0;"> Seniors Mobile Tax Services</a></p>
+                                            <p style="color: #DDDDE0; font-size: 1rem;">Visit <a href=${ process.env.SMTS_Link } style="text-decoration: underline; color: #DDDDE0;"> Seniors Mobile Tax Services</a></p>
                                             <p style="font-size: 0.65rem; text-align: end; color: #DDDDE0;">
                                                 Don't drive... Make the call to Seniors Mobile Tax Services
                                             </p>
@@ -59,10 +59,9 @@ const sendMail = async (req, res) => {
                                     <tr>
                                         <td style="padding: 16px;">
                                             <div style="text-align: center;">
-                                                <hr style="width: 95%; margin: 24px auto; border: 2px solid #AA2B34;">
                                                 <p style="font-size: 0.9rem; margin: 16px auto; width:80%; color: #3C3B6E; min-width: 280px;">Email confirmation from: <br> Seniors Mobile Tax Services <br> We will respond within 1-2 business days</p>
                                                 <h5 style="font-size: 1.3rem; margin: 8px 0 0 0; color: #3C3B6E;">Message received from:</h5>
-                                                <p style="font-size: 1.1rem; margin: 0 0 8px 0; color: #3C3B6E;">${mailData.userName}</p>
+                                                <p style="font-size: 1.1rem; margin: 0 0 16px 0; color: #3C3B6E;">${ mailData.userName }</p>
                                             </div>
                                         </td>
                                     </tr>
@@ -70,7 +69,7 @@ const sendMail = async (req, res) => {
                                         <td style="padding: 8px 16px;">
                                             <div style="text-align: center;">
                                                 <h5 style="font-size: 1.3rem; margin: 8px 0 0 0; color: #3C3B6E;">Contact Email:</h5>
-                                                <p style="font-size: 1.1rem; margin: 0 0 16px 0; color: #3C3B6E;">${mailData.contact}</p>
+                                                <p style="font-size: 1.1rem; margin: 0 0 16px 0; color: #3C3B6E;">${ mailData.contact }</p>
                                             </div>
                                         </td>
                                     </tr>
@@ -78,7 +77,7 @@ const sendMail = async (req, res) => {
                                         <td style="padding: 8px 16px;">
                                             <div style="text-align: center;">
                                                 <h5 style="font-size: 1.3rem; margin: 8px 0 0 0; color: #3C3B6E;">Subject:</h5>
-                                                <p style="font-size: 1.1rem; margin: 0 0 16px 0; color: #3C3B6E;">${mailData.subject}</p>
+                                                <p style="font-size: 1.1rem; margin: 0 0 16px 0; color: #3C3B6E;">${ mailData.subject }</p>
                                             </div>
                                         </td>
                                     </tr>
@@ -86,7 +85,7 @@ const sendMail = async (req, res) => {
                                         <td style="padding: 8px 16px;">
                                             <div style="text-align: center;">
                                                 <h5 style="font-size: 1.3rem; margin: 8px 0 0 0; color: #3C3B6E;">Message:</h5>
-                                                <p style="font-size: 1.1rem; margin: 0 auto 16px; max-width: 300px; color: #3C3B6E;">${mailData.message}</p>
+                                                <p style="font-size: 1.1rem; margin: 0 auto 16px; max-width: 300px; color: #3C3B6E;">${ mailData.message }</p>
                                             </div>
                                         </td>
                                     </tr>
@@ -101,7 +100,7 @@ const sendMail = async (req, res) => {
                                         <td style="padding: 8px;">
                                             <hr style="width: 95%; margin: 8px auto 24px; border: 2px solid #AA2B34;">
                                             <h5 style="margin: 0; padding: 0 8px; color: #3C3B6E;">Disclaimer:</h5>
-                                            <p style="font-size: .8rem ;margin: 8px auto; padding: 0 8px; width:95%; color: #3C3B6E;">This email was intended for ${mailData.userName} (${mailData.contact}). If you are not the intended recipient of this email, please notify the sender immediately by replying to this message and delete this email from your inbox. Any unauthorized use, disclosure, or distribution of this email is prohibited. Thank you for your understanding.</p>
+                                            <p style="font-size: .8rem ;margin: 8px auto; padding: 0 8px; width:95%; color: #3C3B6E;">This email was intended for ${ mailData.userName } (${ mailData.contact }). If you are not the intended recipient of this email, please notify the sender immediately by replying to this message and delete this email from your inbox. Any unauthorized use, disclosure, or distribution of this email is prohibited. Thank you for your understanding.</p>
                                         </td>
                                     </tr>
                                 </table>
